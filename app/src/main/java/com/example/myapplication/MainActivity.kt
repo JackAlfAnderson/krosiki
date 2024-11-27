9cdad15e4eacaa4e626efc525f9da1de7d1cbd88
JE_DOG <gamzat.magomedov809@gmail.com>
2024-11-27 14:13:04 +0300

    split-1 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.presentation.signIn.SignInScreen
+import com.example.myapplication.presentation.splash.SplashScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android", // something
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "splash"
+                ){
+                    composable(route = "splash"){
+                        SplashScreen(navController)
+                    }
+                    composable(route = "signIn"){
+                        SignInScreen()
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/myapplication/presentation/splash/SplashScreen.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication.presentation.splash
+
+import android.os.CountDownTimer
+import android.window.SplashScreen
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.R
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.icon_round),
+            contentDescription = "",
+            tint = Color.Unspecified
+        )
+    }
+    var timer: CountDownTimer = object : CountDownTimer(3000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            val m = millisUntilFinished
+        }
+
+        override fun onFinish() {
+            navController.navigate("signIn")
+        }
+    }.start()
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SplashPreview() {
+    SplashScreen(
+        navController = rememberNavController()
+    )
+}

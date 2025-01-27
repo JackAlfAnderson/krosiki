771bf47e4226392b4be850de3c2f887fdfe183fe
JackAlfAnderson <jackanderson78907890@gmail.com>
2025-01-27 14:41:39 +0300

    kak zhe ya lublu commitit

--- app/src/main/java/com/example/myapplication/presentation/detailsScreen/detailsScreen.kt
+++ app/src/main/java/com/example/myapplication/presentation/detailsScreen/detailsScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
